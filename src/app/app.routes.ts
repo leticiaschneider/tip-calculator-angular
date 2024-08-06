7aad79bcc8dfd5ec7a04882e8e38b18adca6f4b7
leticiaschneider <leticia.schneider@gmail.com>
2024-08-05 21:16:05 -0300

    form component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FormBillComponent } from './form-bill/form-bill.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", component: FormBillComponent}
+];
---
+++ src/app/form-bill/form-bill.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-bill',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-bill.component.html',
+  styleUrl: './form-bill.component.scss'
+})
+export class FormBillComponent {
+
+}
