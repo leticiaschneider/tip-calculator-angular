ee707419f0a3e65d6321de3529aaafed5b3ed24f
leticiaschneider <leticia.schneider@gmail.com>
2024-08-07 22:25:38 -0300

    add google font

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TipCalculatore
+# Tipcalculator
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.5.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'tip-calculatore' title`, () => {
+  it(`should have the 'tip-calculator' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tip-calculatore');
+    expect(app.title).toEqual('tip-calculator');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tip-calculatore');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, tip-calculator');
   });
 });
