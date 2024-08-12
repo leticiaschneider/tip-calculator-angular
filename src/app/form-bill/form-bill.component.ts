59ea149bef27bfbc726aa9955e0060fb9310478f
leticiaschneider <leticia.schneider@gmail.com>
2024-08-12 19:29:27 -0300

    update form with new bindings and variables

--- src/app/form-bill/form-bill.component.ts
+++ src/app/form-bill/form-bill.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-bill',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './form-bill.component.html',
   styleUrl: './form-bill.component.scss'
 })
 export class FormBillComponent {
-
+  billObj = {
+    billValue: 0,
+    tipPercentage: 0,
+    numberOfPeople: 1,
+    tipAmountPerPerson: 0,
+    totalPerPerson: 0
+  }
 }
